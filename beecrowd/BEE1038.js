145cdb9e3a06a4f9a8446445eb7341f515d7e0c7
Ricardo Carubbi <carubbi@unifor.br>
2026-03-06 09:58:43 -0300

    Implement product pricing and total calculation

--- beecrowd/BEE1038.js
+++ beecrowd/BEE1038.js
@@ -1,1 +1,35 @@
+// Declarar as variáveis
+let entradaCod;
+let entradaQtd;
+let codProd;
+let qtdProd;
+let precoProd;
+let totalPagar;
 
+// Entrada de dados
+entradaCod = prompt('Digite o código do produto: ');
+entradaQtd = prompt('Digite a quantidade do produto: ');
+
+// Processamento dos dados
+// Converter as entradas (string) para numérico
+codProd = parseInt(entradaCod);
+qtdProd = parseInt(entradaQtd);
+
+// Verificar o total a pagar
+if (codProd == 1) {
+    precoProd = 4;
+} else if (codProd == 2) {
+    precoProd = 4.4;
+} else if (codProd == 3) {
+    precoProd = 5;
+} else if (codProd == 4) {
+    precoProd = 2;
+} else {
+    precoProd = 1.5;
+}
+
+// Calcular o total a pagar
+totalPagar = qtdProd * precoProd;
+
+// Imprimir o total a pagar
+console.log(`Total: R$ ${totalPagar.toFixed(2)}`);
